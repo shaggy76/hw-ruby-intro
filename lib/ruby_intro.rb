675309f24cc435afc765d5bb9b7374c5ab9daf60
Scott Harrison <shaggy76@github.com>
2017-12-22 08:39:14 +0000

    Complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,76 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  b = arr.sort.reverse
+  if b.length == 0
+    0
+  elsif b.length == 1
+    b[0]
+  else
+    sum(b[0..1])
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  success = false
+  arr.length.times do    
+    f = arr.pop
+    arr.map do |s|
+      if f + s == n
+        success = true
+      end
+    end
+  end
+  success
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /^[aeiou]+/.match(s.downcase) 
+    false
+  elsif /^[a-z]/.match(s.downcase)
+    true
+  else
+    false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /[^01]+/  or s.length == 0
+    false
+  else
+    s.to_i(10) % 4 == 0
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def  initialize(isbn, price)
+    raise ArgumentError if isbn.length == 0
+    @isbn = isbn
+    raise ArgumentError if price <= 0
+    @price = price
+  end
+
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def price_as_string
+    "$%0.2f" % @price
+  end
+
+  
+  
+
+
 end
